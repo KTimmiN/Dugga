82649a2d2de2f1aab8210f8803db48f9d36ae50d
nikqi <timmi_nikqi@hotmail.com>
2015-04-16 09:51:19 +0200

    Added the tests file and starting to implement on it

---
+++ DuggaNumreric.Test/DuggaNumrericTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DuggaNumreric.Test
+{
+    [TestClass]
+    public class DuggaNumrericTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
