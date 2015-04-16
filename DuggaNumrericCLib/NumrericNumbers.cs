12360aeba9b11feeabbef8f78f55b2c1a9edbc5e
nikqi <timmi_nikqi@hotmail.com>
2015-04-16 10:25:15 +0200

    Done with the bad code, all tests succeded

---
+++ DuggaNumrericCLib/NumrericNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DuggaNumreric
+{
+    public class NumrericNumbers
+    {
+        public string DuggaNumreric(string message)
+        {
+            if(message == null)
+            {
+                throw new ArgumentNullException("Message is null");
+            }
+            if (message.Trim().Equals(""))
+            {
+                throw new ArgumentException("Message is empty");
+
+            }
+            if (message.Trim().Equals("k"))
+            {
+                return "1000";
+            }
+            if (message.Trim().Equals("1k"))
+            {
+                return "1000";
+            }
+            return message;
+        }
+    }
+}
